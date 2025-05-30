09c352faa820d0528a00c08c8af5e5cc8b30db50
mukiibi <ibrahimmukiibi803@gmail.com>
2025-05-30 10:27:04 +0300

    Update 1.Compute_Simple_Interest.c
    print simple interest

--- 1.Compute_Simple_Interest.c
+++ 1.Compute_Simple_Interest.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+ #define Intrest_Rate 0.15
+int main()
+{
+ double principal = 50000; 
+ double time = 3;
+ double Simple_interest = principal*time*Intrest_Rate;
+
+ printf("The Simple interest is %.2lf\n\n",Simple_interest);
+
+ return 0;   
+}
 
